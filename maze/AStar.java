4858f7d7180b0deda1ebd047f6fb1cfac32ba5ca
Derrick Harrill <dh000113@sbuniv.edu>
2018-03-13 04:09:33 +0000

    reupload project

---
+++ maze/AStar.java
@@ -0,0 +1,27 @@
+package maze;
+
+public class AStar<T> implements Agenda<T>{
+    private Heap<T> heap;
+    
+    public AStar(){
+        heap = new Heap<T>();
+    }
+    
+    public boolean isEmpty(){
+        return heap.isEmpty();
+    }
+    
+    public int size(){
+        return heap.size();
+    }
+    
+    public void add(T newItem){
+        heap.insert(newItem);
+    }
+    public T remove(){
+        return heap.remove();
+    }
+    public T peek(){
+        return heap.peek();
+    }
+}
